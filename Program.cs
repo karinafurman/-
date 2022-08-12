c2fa1b922b01ef74bf7ad572b57e2e2583116ab9
Karina <karina-karina-furman-furman@mail.ru>
2022-08-13 00:32:03 +0300

    Созданы инициализация и вывод массива строк

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача: написать программу, которая из имеющегося массива строк формирует
+// массив из строк, длина которых меньше либо равна 3.
+
+string[] StringMass = {"Friend", "22", "Sun", "Friday", "Fairy", "Program", "Git", "Summer"};
+for (int i = 0; i < StringMass.Length; i++)
+{
+Console.WriteLine("SM[{0}] = {1}", i, StringMass[i]);
+Console.ReadKey();
+}
